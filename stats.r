667eff3074f4f9cf6642b79577c5415797e71804
Aarav <68392333+BIGBRAINZZ@users.noreply.github.com>
2021-07-06 12:32:40 -0700

    Create stats.r

---
+++ stats.r
@@ -0,0 +1,14 @@
+
+# R program to illustrate
+# taking input from the user
+  
+# taking input using readline()
+# this command will prompt you
+# to input a desired value
+var = readline();
+  
+# convert the inputted value to integer
+var = as.integer(var);
+  
+# print the value
+print(var)
